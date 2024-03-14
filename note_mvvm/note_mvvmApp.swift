a15e31ff116487f21fd4ca0f6db25b0d8c9e0856
linhdth <linhdth.1989@gmail.com>
2024-03-15 00:47:43 +0700

    Initial Commit

---
+++ note_mvvm/note_mvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  note_mvvmApp.swift
+//  note_mvvm
+//
+//  Created by apple on 15/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct note_mvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
